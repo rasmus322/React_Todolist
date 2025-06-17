80acb606a82b0e37ee1c1943bd69aec462232424
rasmus322 <saltanovvlad@yandex.ru>
2025-06-17 17:22:12 +0500

    added filter option for Categories Component
    added filter option for TodoCategories Component

--- src/components/TodoCategories/index.tsx
+++ src/components/TodoCategories/index.tsx
@@ -1,20 +1,35 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/mainStore';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, setSelectedCategory } from '../../store/mainStore';
 import './style.css';
 
 const TodoCategories = () => {
     const todos = useSelector((state: RootState) => state.todos.todos);
     const categories = useSelector((state: RootState) => state.todos.categories);
+    const selectedCategory = useSelector((state: RootState) => state.todos.selectedCategory);
+    const dispatch = useDispatch();
     
     const categoryCounts: Record<string, number> = {};
     todos.forEach(todo => {
         categoryCounts[todo.category] = (categoryCounts[todo.category] || 0) + 1;
     });
     
+    const handleCategoryClick = (category: string) => {
+        if (selectedCategory === category) {
+            dispatch(setSelectedCategory(null)); // Deselect if already selected
+        } else {
+            dispatch(setSelectedCategory(category));
+        }
+    };
+    
     return (
         <div className='category-container'>
             {Object.entries(categories).map(([key, value]) => (
-                <div key={key} className="category-item" style={{ backgroundColor: `var(--todoItem-category-bg-${key})` }}>
+                <div 
+                    key={key} 
+                    className={`category-item ${selectedCategory === key ? 'selected' : ''}`}
+                    style={{ backgroundColor: `var(--todoItem-category-bg-${key})` }}
+                    onClick={() => handleCategoryClick(key)}
+                >
                     <img className='category-img' src={value.icon} alt={value.text} />
                     <div className='category-text'>
                         <b>{categoryCounts[key] || 0}</b>
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -4,14 +4,19 @@ import { RootState } from '../../store/mainStore';
 import './style.css';
 
 const TodoList = () => {
-    const todoItems = useSelector((state: RootState) => state.todos.todos);
+    const todos = useSelector((state: RootState) => state.todos.todos);
+    const selectedCategory = useSelector((state: RootState) => state.todos.selectedCategory);
+
+    const filteredTodos = selectedCategory 
+        ? todos.filter(todo => todo.category === selectedCategory)
+        : todos;
 
     return (
-        todoItems.length === 0 ? (
+        filteredTodos.length === 0 ? (
             <p>На данный момент задач нет</p>
         ) : (
             <ul className='list'>
-                {todoItems.map((item) => (
+                {filteredTodos.map((item) => (
                     <TodoItem key={item.id} todoItem={item} />
                 ))}
             </ul>
